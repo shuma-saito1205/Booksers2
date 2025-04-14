07fcb6a077a0da75ecec25647df748e90a1b5883
shumasaito <shushushu1205@gmail.com>
2025-04-14 11:50:01 +0000

    [Add]クリエイト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :image
-  
+  has_many :post_images, dependent: :destroy
+
+
 end
