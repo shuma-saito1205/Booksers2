0b7496d15c8f22a15c82ea15cb3afe256107d469
shumasaito <shushushu1205@gmail.com>
2025-04-17 08:29:09 +0000

    [Add]booksへアソシエーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book)
     else
       render 'new'
     end
