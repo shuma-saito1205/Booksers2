34dbbb559cbee02c967945f7bf908c50c9bf4bb9
shumasaito <shushushu1205@gmail.com>
2025-04-18 05:39:52 +0000

    [Add]ベース完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def index
