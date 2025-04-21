4c8414a7a1a1cdbb5a98ae54407a7b262d16a971
shumasaito <shushushu1205@gmail.com>
2025-04-21 07:18:33 +0000

    [Add]アクセス制限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+before_action :is_matching_login_user, only: [:edit, :update, :destroy]
+
   def new
     @book = Book.new
   end
@@ -50,6 +52,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
