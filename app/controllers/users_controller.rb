5025d21ad2025fa99b8c62be0827758d8e0b1eb6
shumasaito <shushushu1205@gmail.com>
2025-04-25 03:14:15 +0000

    [Add]RSpecテスト,ユーザー情報編集リンクのみ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
-      redirect_to books_path(@user.id)
+      redirect_to user_path(@user)
     else
       render "edit"
     end
